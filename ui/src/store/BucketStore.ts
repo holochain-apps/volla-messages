5322875bd320cbce0b4c09cf142d2c4727da1892
Matt Gabrenya <matt@buildyourweb.app>
2024-12-24 21:28:47 -0700

    refactor(ui): convert BucketStore and MessageHistoryStore to svelte custom stores, instead of classes. Don't wrap child custom stores in writables.

--- ui/src/store/BucketStore.ts
+++ ui/src/store/BucketStore.ts
@@ -1,27 +1,54 @@
-import type { ActionHashB64 } from "@holochain/client";
-import { get, writable, type Writable } from "svelte/store";
+import type { ActionHashB64, DnaHashB64 } from "@holochain/client";
+import { persisted } from "svelte-persisted-store";
+import { derived, get, type Invalidator, type Subscriber, type Unsubscriber } from "svelte/store";
 
-export class BucketStore {
-  public hashes: Writable<Set<ActionHashB64>> = writable(new Set());
+export interface BucketStore {
+  add: (hashes: ActionHashB64[]) => void;
+  missingHashes: (allHashes: ActionHashB64[]) => ActionHashB64[];
+  subscribe: (
+    this: void,
+    run: Subscriber<{
+      hashes: string[];
+      count: number;
+    }>,
+    invalidate?:
+      | Invalidator<{
+          hashes: string[];
+          count: number;
+        }>
+      | undefined,
+  ) => Unsubscriber;
+}
 
-  constructor(hashes: Array<ActionHashB64>) {
-    this.hashes.set(new Set(hashes));
+export function createBucketStore(
+  dnaHashB64: DnaHashB64,
+  bucket: number,
+  initialHashes: ActionHashB64[] = [],
+) {
+  const hashes = persisted(
+    `CONVERSATIONS.${dnaHashB64}.BUCKETS.${bucket}`,
+    Array.from(new Set(initialHashes)),
+    { storage: "local" },
+  );
+
+  const { subscribe } = derived(hashes, ($hashes) => {
+    return {
+      hashes: $hashes,
+      count: $hashes.length,
+    };
+  });
+
+  function add(newHashes: ActionHashB64[]) {
+    hashes.update(($hashes) => Array.from(new Set([...$hashes, ...newHashes])));
   }
 
-  get count(): number {
-    return get(this.hashes).size;
+  function missingHashes(allHashes: ActionHashB64[]): ActionHashB64[] {
+    return Array.from(new Set(allHashes).difference(new Set(get(hashes))));
   }
 
-  toJSON(): string {
-    return JSON.stringify(Array.from(get(this.hashes)!.keys()));
-  }
-
-  add(newHashes: Array<ActionHashB64>) {
-    this.hashes.update((h) => new Set([...h, ...newHashes]));
-  }
-
-  missingHashes(hashes: Array<ActionHashB64>): Array<ActionHashB64> {
-    const s: Set<ActionHashB64> = new Set(hashes);
-    return Array.from(s.difference(get(this.hashes)));
-  }
+  return {
+    add,
+    missingHashes,
+    subscribe,
+  };
 }
