6d833874d4342172b9bf1f268c83b08c42bfc11d
Matt Gabrenya <matt@buildyourweb.app>
2024-12-24 17:05:41 -0700

    chore(ui): use page params directly

--- ui/src/routes/contacts/[id]/+page.svelte
+++ ui/src/routes/contacts/[id]/+page.svelte
@@ -2,10 +2,8 @@
   import { page } from "$app/stores";
   import Header from "$lib/Header.svelte";
   import ContactEditor from "../ContactEditor.svelte";
-
-  $: contactId = $page.params.id;
 </script>
 
 <Header backUrl="/create" />
 
-<ContactEditor agentPubKeyB64={contactId} />
+<ContactEditor agentPubKeyB64={$page.params.id} />
