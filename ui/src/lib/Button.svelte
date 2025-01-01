179ff39c53da9efaea34819e82aa1d236084e9f7
Matt Gabrenya <matt@buildyourweb.app>
2025-01-01 12:04:59 -0700

    refactor(ui): use flex-row-reverse for button alignment

--- ui/src/lib/Button.svelte
+++ ui/src/lib/Button.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import SvgIcon from "$lib/SvgIcon.svelte";
-  import { Alignment } from "./types";
+  import { Alignment } from "$lib/types";
 
   export let moreClasses = "";
   export let loading = false;
@@ -11,27 +11,18 @@
 
 <button
   class="variant-filled-primary dark:variant-filled-tertiary flex h-12 w-72 items-center rounded-full px-5 py-2 disabled:opacity-50
+  {iconAlign === Alignment.Left ? 'flex-row' : 'flex-row-reverse'}
   {moreClasses}"
   on:click
   {...$$restProps}
 >
-  {#if iconAlign === Alignment.Left}
-    {#if loading}
-      <SvgIcon icon="spinner" size={iconSize} color="%23FD3524" />
-    {:else if icon !== undefined}
-      <SvgIcon {icon} size={iconSize} color="%23FD3524" />
-    {/if}
+  {#if loading}
+    <SvgIcon icon="spinner" size={iconSize} color="%23FD3524" />
+  {:else if icon !== undefined}
+    <SvgIcon {icon} size={iconSize} color="%23FD3524" />
   {/if}
 
   <div class="flex w-full justify-center font-bold">
     <slot></slot>
   </div>
-
-  {#if iconAlign === Alignment.Right}
-    {#if loading}
-      <SvgIcon icon="spinner" size={iconSize} color="%23FD3524" />
-    {:else if icon !== undefined}
-      <SvgIcon {icon} size={iconSize} color="%23FD3524" />
-    {/if}
-  {/if}
 </button>
