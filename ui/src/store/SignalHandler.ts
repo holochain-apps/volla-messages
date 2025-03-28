c25c17b15d648e8d7c6528c1f8473038cdd02fff
MasterK0927 <keshav.agarwal.12325@gmail.com>
2025-03-29 02:38:37 +0530

    revert: revert signal handler change, pushed by mistake

--- ui/src/store/SignalHandler.ts
+++ ui/src/store/SignalHandler.ts
@@ -1,4 +1,4 @@
-import { encodeHashToBase64, type AppSignal } from "@holochain/client";
+import { encodeHashToBase64, type Signal, SignalType } from "@holochain/client";
 import { RelayClient } from "$store/RelayClient";
 import { type RelaySignal, type MessageSignal } from "$lib/types";
 import { encodeCellIdToBase64 } from "$lib/utils";
@@ -15,15 +15,16 @@ export function createSignalHandler(
 ) {
   client.client.on("signal", _handleSignalReceived);
 
-  async function _handleSignalReceived(signal: AppSignal) {
+  async function _handleSignalReceived(signal: Signal) {
+    if (!(SignalType.App in signal)) return;
 
-    const payload = signal.payload as RelaySignal;
-    const cellIdB64 = encodeCellIdToBase64(signal.cell_id);
+    const payload = signal[SignalType.App].payload as RelaySignal;
+    const cellIdB64 = encodeCellIdToBase64(signal[SignalType.App].cell_id);
 
     if (payload.type === "Message") {
       await conversationMessageStore.handleMessageSignalReceived(
         cellIdB64,
-        signal.payload as MessageSignal,
+        signal[SignalType.App].payload as MessageSignal,
       );
       // Mark conversation as unread
       // Unless user is currently viewing the conversation page.
