53c929053f22379ccd75c7eb75d0a721dc76896b
Eric Harris-Braun <eric@harris-braun.com>
2025-10-08 15:57:13 -0400

    fix: update android ci to us rust 1.88

--- flake.nix
+++ flake.nix
@@ -40,6 +40,13 @@
 
           };
           devShells.androidDev = pkgs.mkShell {
+            packages = (with inputs'.holonix.packages; [
+              rust # For Rust development, with the WASM target included for zome builds
+            ]) ++ (with pkgs; [
+              nodejs_20 # For UI development
+              binaryen # For WASM optimisation
+              # Add any other packages you need here
+            ]);
             inputsFrom = [
               inputs'.p2p-shipyard.devShells.holochainTauriAndroidDev
               inputs'.holonix.devShells.default
